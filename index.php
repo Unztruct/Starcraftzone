13e86e18607ce797cd170d7d49dcf6065fd24d54
vilnys380 <vilnys380@E29650.edu.linkoping.se>
2014-12-08 10:58:03 +0100

    Teenage mutant ninja turtles

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 
         <?php
-       
+       define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "elefanter");
+
+
+$dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
         
         
         
